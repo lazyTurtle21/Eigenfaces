b4320e0099e8f3eeaa751f26bab4a62eb6e68bff
Andriy <Andriyd909@gmail.com>
2019-05-07 01:45:45 +0300

    Started adding face detection

--- image.py
+++ image.py
@@ -9,26 +9,35 @@ class Image:
 
     @classmethod
     def read_image(cls, path):
+        image = cls.read_image_2d(path)
+
+        cls.shapes[image.shape[0] * image.shape[1]] = image.shape
+        image = image.flatten()
+        return image
+
+    @classmethod
+    def read_image_2d(cls, path):
         image = mpimage.imread(path)
 
-        if len(image.shape) == 3 and image.shape[2] == 3:
+        if len(image.shape) == 3 and image.shape[2] >= 3:
             r, g, b = image[:, :, 0], image[:, :, 1], image[:, :, 2]
             image = 0.2989 * r + 0.5870 * g + 0.1140 * b
         elif len(image.shape) != 2:
             raise Exception("Invalid image dimensions: " + str(image.shape))
 
-        cls.shapes[image.shape[0] * image.shape[1]] = image.shape
-
-        image = image.flatten()
         image = image / 255
-
         return image
 
     @classmethod
     def show_image(cls, image):
         # get original shape
-        image = np.reshape(image, cls.shapes[image.shape[0]])
+        if len(image.shape) == 1:
+            image = np.reshape(image, cls.get_image_shape(image))
 
         # display the image
         plt.imshow(image, interpolation='nearest', cmap=plt.get_cmap("gray"))
         plt.show()
+
+    @classmethod
+    def get_image_shape(cls, image):
+        return cls.shapes[image.shape[0]]
