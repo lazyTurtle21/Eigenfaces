69bf5d960e95e8dd45f703276cf8a8c26d6f1e6d
Andriy <Andriyd909@gmail.com>
2019-05-07 01:55:02 +0300

    Removed trash

--- person.py
+++ person.py
@@ -1,40 +1,6 @@
-<<<<<<< HEAD
 import os
 import numpy as np
 
-from image import Image
-
-
-class Person:
-    def __init__(self, path, eigenfaces):
-        self.name = path.split("/")[-1]
-        self.number_pic = 0
-        self.class_vector = None
-        self.eigenfaces = eigenfaces
-
-        # calculating representing class vector
-        self.initialize(path)
-
-    def initialize(self, path):
-        weights = []
-
-        # calculating weights for all images of given person
-        if os.path.isdir(path):
-            for image in os.listdir(path):
-                image = Image.read_image(path + "/" + image) - self.eigenfaces.average
-                weights.append(self.eigenfaces.calculate_weight(image))
-        weights = np.array(weights)
-
-        # take the average over weights
-        self.class_vector = np.mean(weights, axis=0)
-        self.number_pic = weights.shape[0]
-
-    def distance(self, person2):
-        return np.linalg.norm(self.class_vector - person2)
-=======
-import os
-import numpy as np
-from scipy.stats import norm
 
 from image import Image
 
@@ -61,4 +27,3 @@ class Person:
     def distance(self, other_weight):
         return np.linalg.norm(self.class_weight - other_weight)
 
->>>>>>> 1f7c8d25cb31c51171a318e89d368313513ed870
