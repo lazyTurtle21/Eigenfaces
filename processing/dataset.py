e9061d1bb84dc45ccf0b8ecb4b890f195e018e27
Andriy Dmytruk <Andriyd909@gmail.com>
2019-05-14 10:33:38 +0300

    Added error correction to dataset

--- processing/dataset.py
+++ processing/dataset.py
@@ -14,6 +14,24 @@ class Dataset:
         self.average_dist = None
         self.standard_deviation = None
 
+    def normalize_people(self):
+        self.persons = [*filter(lambda x: len(x.images) > 0, self.persons)]
+
+        number_images = len(self.all_images)
+        if number_images > 0:
+            shape = self.all_images[0].shape
+
+            def filter_shape(images):
+                return np.array([*filter(lambda i: i.shape == shape, images)])
+
+            self.all_images = filter_shape(self.all_images)
+            for person in self.persons:
+                person.images = filter_shape(person.images)
+
+        if len(self.all_images) != number_images:
+            print("[Eigenfaces Warning] %d images did not match dimensions" %
+                  (number_images - len(self.all_images)))
+
     def initialize(self, directory):
         persons = []
         all_images = []
@@ -22,11 +40,14 @@ class Dataset:
             person_directory_path = directory + "/" + person_directory
             if os.path.isdir(person_directory_path):
                 person = Person(person_directory_path)
+
                 persons.append(person)
                 all_images.extend(person.images)
 
         self.persons = np.array(persons)
         self.all_images = np.array(all_images)
+
+        self.normalize_people()
 
         print("[Eigenfaces] Images loaded", self.all_images.shape[0])
 
